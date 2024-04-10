ff80ecc2e32b5da388529947a3e7a795003e3ec4
Jdka1 <staryan.mehra@gmail.com>
2024-04-10 16:23:16 -0700

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
-import streamlit as st
-
-st.write("Hello World")
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from google.colab import files
+import requests
+from io import BytesIO
+import zipfile
+import io
