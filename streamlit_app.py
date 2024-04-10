38b3fe0ad674787197401e707f6b1c4f18b87d00
Jdka1 <staryan.mehra@gmail.com>
2024-04-10 16:27:45 -0700

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
-from google.colab import files
+# from sklearn.preprocessing import MinMaxScaler
+# from google.colab import files
 import requests
 from io import BytesIO
 import zipfile
